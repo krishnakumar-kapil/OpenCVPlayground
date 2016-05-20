8c2c93e29a05ac3faf615e8a28a27a25fe8b2d6c
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2016-05-20 13:08:04 +0530

    added terminal options

--- SeamCarving/main.cpp
+++ SeamCarving/main.cpp
@@ -1,8 +1,17 @@
 
 #include "SeamCarver.h"
+#include <stdio.h>
+#include <cstring>
+int main(int argc, char **argv){
+    if(argc < 4){
+        cout << "Please provide more arguments\n";
+        return -1;
+    }
+    string title = argv[1];
+    int width = atoi(argv[2]);
+    int height = atoi(argv[3]);
 
-int main(){
-    Mat_<Vec3b> image = imread("bench.jpg");
+    Mat_<Vec3b> image = imread(title);
 //  Mat_<Vec3b> image = imread("lighthouse.jpg");
 //  Mat_<Vec3b> image = imread("bench.jpg");
     if (!image.data) {
@@ -14,12 +23,19 @@ int main(){
     imshow("Original Image", image);
     SeamCarver s(image);
 
-	for (int i = 0; i < 160; ++i) {
+	for (int i = 0; i < width; ++i) {
 		vector<uint> seam = s.findVerticalSeam();
 //		s.showHorizontalSeam(seam);
 		s.removeVerticalSeam(seam);
 	}
+
+	for (int j = 0; j < height; ++j) {
+		vector<uint> seam = s.findHorizontalSeam();
+//		s.showHorizontalSeam(seam);
+		s.removeHorizontalSeam(seam);
+	}
 	imshow("Carved Image", s.getImage());
+    cout << "Press any key to continue\n";
 	waitKey(0);
 	image.release();
     return 0;
