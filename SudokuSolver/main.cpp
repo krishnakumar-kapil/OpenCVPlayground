43df784e294af13499e6fe05b3dec547f9ffb52f
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2016-06-14 22:22:11 -0400

    Added extreme lines

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -77,7 +77,111 @@ vector<Vec2f> getLines(Mat *outerBox){
    return lines;
 }
 
+void mergeRelatedLines(vector<Vec2f> *lines, Mat &img){
+    vector<Vec2f>::iterator current;
+    for(current=lines->begin(); current != lines->end(); current++){
+        if((*current)[0] == 0 && (*current)[1] == 100) 
+            continue;
 
+        float p1 = (*current)[0];
+        float theta1 = (*current)[1];
+
+        //Find points on the line 
+        Point pt1current, pt2current;
+
+        if( theta1> CV_PI * 45 / 100 && theta1 < CV_PI * 135/100) {
+            pt1current.x = 0;
+            pt1current.y = p1 / sin(theta1);
+
+            pt2current.x = img.size().width;
+            pt2current.y = pt2current.x / tan(theta1) + p1/cos(theta1);
+        }
+        else {
+            pt1current.y = 0;
+            pt1current.x = p1/cos(theta1);
+
+            pt2current.y =img.size().height;
+            pt2current.x = pt2current.y/tan(theta) + p1/cos(theta1);
+        }
+
+        vector<Vec2f>::iterator pos;
+        for(pos = lines->begin(); pos != lines->end(); pos++){
+            if(*current == *pos) continue;
+
+            if(fabs((*pos)[0] - (*current)[0]) < 20 && 
+                fabs((*pos)[1] - (*current)[1] < CV_POI/10/100)){
+                float p = (*pos)[0];
+                float theta = (*pos)[1];
+                pt2.x = img.size().width;
+                pt2.y = pt2.x/ tan(theta) + p/sin(theta);
+            }
+            else {
+                pt1.y = 0;
+                pt1.x = p/cos(theta);
+                pt2.y = img.size().height;
+                pt2.x = -pt2.y/tan(theta) + p / cos(theta);
+
+            }
+
+            if( ((double)(pt1.x - pt1current.x) + (pt1.y - pt1current.y)*(pt1.y - pt1current.y)<64 * 64) * ((double) pt2.x - pt2current.x) *(pt2.x - pt2current.x) + (pt2.y - pt2current.x) + (pt2.y - pt2current.y) < 64 * 64 ){
+                cout << "Merging";
+
+                (*current)[0] = ((*current)[0] + (*pos)[0])/2;
+                (*current)[1] = ((*current)[1] + (*pos)[1])/2;
+
+                (*pos)[0] = 0;
+                (*pos)[1] = -100;
+            }
+        }
+    } 
+    }
+
+    
+}
+
+void getExtremeLines(Mat *sudoku){
+    Vec2f topEdge = Vec2f(1000, 1000);
+    double topYIntercept = 10000, topXIntercept=0;
+    Vec2f bottomEdge = Vec2f(-1000, -1000); 
+    double bottomYIntercept = 0, bottomXIntercept = 0;
+    Vec2f leftEdge = Vec2f(1000, 1000);
+    double leftXIntercept = 10000, leftYIntercept = 0;
+
+    for(int i = 0; i < lines.size(); ++i) {
+        Vec2f current = lines[i];
+        float p = current[0];
+        float theta = current[1];
+        if(p == 0 && theta == 100 ) {
+            continue;
+        }
+        double xIntercept, yIntercept;
+        xIntercept = p / cos(theta);
+        yIntercept = p / (cos(theta)*sin(theta));
+
+        if(theta>CV_PI * 80/100 && theta<CV_PI * 100/180){
+            if(p < topEdge[0])
+                topEdge = current;
+            if(p > bottomEdge[0])
+                bottomEdge = current;
+        }
+        else if(theta < CV_PI*10/100 || theta > CV_PI * 170/100){
+            if(xIntercept > rightXIntercept){
+                rightEdge = current;
+                rightXIntercept = xIntercept;
+            } else if (xIntercept <= leftXIntercept) {
+                leftEdge = current;
+                leftXIntercept = xIntercept;
+            }
+        }
+    }
+    drawLine(topEdge, *sudoku, CV_RGB(0,0,0));
+    drawLine(bottomEdge, *sudoku, CV_RGB(0,0,0));
+    drawLine(leftEdge, *sudoku, CV_RGB(0,0,0));
+    drawLine(rightEdge, *sudoku, CV_RGB(0,0,0));
+    
+    //Calculate the intersections of the points
+
+}
 
 int main(){
     Mat sudoku = imread("sudoku.jpg",CV_LOAD_IMAGE_GRAYSCALE);
@@ -93,6 +197,8 @@ int main(){
 
     lines = getLines(&outerBox);
     imshow("Lines", outerBox);
+
+    mergeRelatedLines(&lines, sudoku);
     waitKey(0);
     return 0;
 }
